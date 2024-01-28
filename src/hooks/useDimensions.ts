458d167ad26bacf1163c388903ce76527b8db877
jeresc <jeremiastomassrc@gmail.com>
2024-01-28 18:59:09 -0300

    parrallax scroll slider finished 🎉

---
+++ src/hooks/useDimensions.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export const useDimensions = () => {
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+
+  const updateDimensions = () => {
+    const { innerWidth, innerHeight } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+  };
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, []);
+
+  return dimensions;
+};
